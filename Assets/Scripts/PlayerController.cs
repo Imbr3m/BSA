8d9b18984f4a2dff83046cb7f343abd1c5b98d2c
Rainer Santos <rainertoviel@gmail.com>
2026-04-18 16:47:25 +0400

    added new sprite assets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,7 +30,6 @@ public class PlayerController : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float x = playerControls.Player.Move.ReadValue<Vector2>().x;
@@ -47,7 +46,7 @@ public class PlayerController : MonoBehaviour
         anim.SetBool(IS_WALK_PARAM, movement != Vector3.zero);
         
 
-        // freeze the player when dialogue
+        // freeze player when dialogue
         if (DialogueManager.Instance != null && DialogueManager.Instance.PlayingDialogue)
         {
             anim.SetBool(IS_WALK_PARAM, false); // Go to idle animation
